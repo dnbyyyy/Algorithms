63bc4602f4cc2f812375c335f53c4bcb2f625d55
D By <dnbyyyy@niuitmo.ru>
2021-09-28 18:50:05 +0300

    task D done and some tries on C task

--- 2ndLab/src/taskC.java
+++ 2ndLab/src/taskC.java
@@ -1,52 +1,41 @@
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class taskC {
-    static long merge(int[] arr, int[] left, int[] right) {
-        int i = 0, j = 0, count = 0;
-        while (i < left.length || j < right.length) {
-            if (i == left.length) {
-                arr[i+j] = right[j];
-                j++;
-            } else if (j == right.length) {
-                arr[i+j] = left[i];
-                i++;
-            } else if (left[i] <= right[j]) {
-                arr[i+j] = left[i];
-                i++;
-            } else {
-                arr[i+j] = right[j];
-                count += left.length-i;
-                j++;
+    public static void merge(
+            int[] a, int[] l, int[] r, int left, int right) {
+
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                a[k++] = l[i++];
+            }
+            else {
+                a[k++] = r[j++];
             }
         }
-        return count;
-    }
-
-    static long invCount(int[] arr) {
-        if (arr.length < 2)
-            return 0;
-
-        int m = (arr.length + 1) / 2;
-        int[] left = Arrays.copyOfRange(arr, 0, m);
-        int[] right = Arrays.copyOfRange(arr, m, arr.length);
-
-        return invCount(left) + invCount(right) + merge(arr, left, right);
-    }
-
-    public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(new FileReader("inversions.in"));
-        int n = in.nextInt();
-        int[] data = new int[n];
-        for (int i = 0; i < n; i++) {
-            data[i] = in.nextInt();
+        while (i < left) {
+            a[k++] = l[i++];
         }
-        FileWriter writer = new FileWriter("inversions.out");
-        writer.write(String.valueOf(invCount(data)));
-        in.close();
-        writer.close();
+        while (j < right) {
+            a[k++] = r[j++];
+        }
+    }
+
+    public static void mergeSort(int[] a, int n) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+
+        merge(a, l, r, mid, n - mid);
     }
 }
