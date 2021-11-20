d2af0e6c7e2b23b9d833ca9379f1ae3a57968ece
D By <dnbyyyy@niuitmo.ru>
2021-11-20 11:51:07 +0300

    lab 5 done

--- 5thLab/src/taskC.java
+++ 5thLab/src/taskC.java
@@ -5,150 +5,105 @@ import java.util.Scanner;
 
 public class taskC {
 
-    static class BinarySearchTree {
+    static class Node {
+        int key;
+        Node l, r;
 
-        static class Node {
-            int key;
-             Node right;
-             Node left;
-             Node parent;
+        public Node(int key, Node l, Node r) {
+            this.key = key;
+            this.l = l;
+            this.r = r;
+        }
+    }
 
-            public Node(int key) {
-                this.key = key;
-                left = null;
-                right = null;
-                parent = null;
+    static boolean search(Node x, int k) {
+        if (x == null) return false;
+        if (x.key == k) return true;
+        if (k < x.key) return search(x.l, k);
+        else return search(x.r, k);
+    }
+
+    static Node insert(Node x, int a) {
+            if (x == null) return new Node(a, null, null);
+            else if (x.key > a) x.l = insert(x.l, a);
+            else if (x.key < a) x.r = insert(x.r, a);
+        return x;
+    }
+
+    static Node next(Node root, int x) {
+        Node current = root;
+        Node successor = null;
+        while (current != null) {
+            if (current.key > x) {
+                successor = current;
+                current = current.l;
             }
+            else current = current.r;
         }
+        return successor;
+    }
 
-        Node root = null;
-
-        void insert(Node x, int key) {
-            Node z = new Node(key);
-
-            if (root == null) {
-                root = z;
-                return;
+    static Node prev(Node root, int x) {
+        Node current = root;
+        Node successor = null;
+        while (current != null) {
+            if (current.key < x) {
+                successor = current;
+                current = current.r;
             }
-
-            while (x != null) {
-                if (z.key > x.key) {
-                    if (x.right != null) x = x.right;
-                    else  {
-                        z.parent = x;
-                        x.right = z;
-                        break;
-                    }
-                }
-                else if (z.key < x.key) {
-                    if (x.left != null) x = x.left;
-                    else {
-                        z.parent = x;
-                        x.left = z;
-                        break;
-                    }
-                }
-            }
+            else  current = current.l;
         }
+        return successor;
+    }
 
-        boolean search(Node x, int k) {
-            if (x == null) return false;
+    static Node min(Node x) {
+        if (x.l == null) return x;
+        return min(x.l);
+    }
 
-            if (x.key == k) return true;
-
-            if (k < x.key) return search(x.left, k);
-
-            else return search(x.right, k);
+    static Node delete(Node root, int a) {
+        if (root == null) return root;
+        if (a < root.key) root.l = delete(root.l, a);
+        else if (a > root.key) root.r = delete(root.r, a);
+        else if (root.l != null && root.r != null) {
+            root.key = min(root.r).key;
+            root.r = delete(root.r,root.key);
         }
-
-        Node minimum(Node x) {
-            if (x == null) return null;
-            if (x.left == null) return x;
-            return minimum(x.left);
+        else {
+            if (root.l != null) root = root.l;
+            else root = root.r;
         }
-
-        Node maximum(Node x) {
-            if (x == null) return null;
-            if (x.right == null) return x;
-            return maximum(x.right);
-        }
-
-        Node next(Node root, int key) {
-
-            Node current = root;
-            Node successor = null;
-
-            while (current != null) {
-                if (current.key > key) {
-                    successor = current;
-                    current = current.left;
-                }
-                else current = current.right;
-            }
-            return successor;
-        }
-
-        Node prev(Node root, int key) {
-
-            Node current = root;
-            Node successor = null;
-
-            while (current != null) {
-                if (current.key < key) {
-                    successor = current;
-                    current = current.right;
-                }
-                else  current = current.left;
-            }
-            return successor;
-        }
-
-        Node delete(Node current, int z) {
-            if (current == null) return null;
-
-            if (z < current.key) current.left = delete(current.left, z);
-
-            else if (z > current.key) current.right = delete(current.right, z);
-
-            else if (current.left != null && current.right != null) {
-                current.key = minimum(current.right).key;
-                current.right = delete(current.right, current.key);
-            }
-            else {
-                if (current.left != null) current = current.left;
-                else if (current.right != null) current = current.right;
-                else current = null;
-            }
-            return current;
-        }
+        return root;
     }
 
     public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(new FileReader("bstsimple.in"));
         FileWriter out = new FileWriter("bstsimple.out");
-        BinarySearchTree tree = new BinarySearchTree();
+        Node tree = null;
         while (in.hasNext()) {
-            String[] cmd = in.nextLine().split(" ");
-            if (cmd[0].equals("insert")) {
-                tree.insert(tree.root, Integer.parseInt(cmd[1]));
+            String[] buf = in.nextLine().split(" ");
+            String cmd = buf[0];
+            int value = Integer.parseInt(buf[1]);
+            if (cmd.equals("insert")) {
+                if (!search(tree, value)) {
+                    tree = insert(tree, value);
+                }
             }
-            if (cmd[0].equals("delete")) {
-                if (tree.root != null) tree.delete(tree.root, Integer.parseInt(cmd[1]));
+            if (cmd.equals("exists")) {
+                if (search(tree, value)) out.write("true\n");
+                else out.write("false\n");
             }
-            if (cmd[0].equals("exists")) {
-                out.write(Boolean.toString(tree.search(tree.root, Integer.parseInt(cmd[1]))) + '\n');
+            if (cmd.equals("next")) {
+                Node tmp = next(tree, value);
+                if (tmp == null) out.write("none\n");
+                else out.write(String.format("%d\n", tmp.key));
             }
-            if (cmd[0].equals("prev")) {
-
-                String ans = tree.prev(tree.root, Integer.parseInt(cmd[1])) != null ? String.valueOf(tree.prev(tree.root, Integer.parseInt(cmd[1])).key) + '\n' : "none\n";
-
-                out.write(ans);
+            if (cmd.equals("prev")) {
+                Node tmp = prev(tree, value);
+                if (tmp == null) out.write("none\n");
+                else out.write(String.format("%d\n", tmp.key));
             }
-            if (cmd[0].equals("next")) {
-                String ans = tree.next(tree.root, Integer.parseInt(cmd[1])) != null ? String.valueOf(tree.next(tree.root, Integer.parseInt(cmd[1])).key) + '\n' : "none\n";
-
-                out.write(ans);
-            }
+            if (cmd.equals("delete")) tree = delete(tree, value);
         }
         in.close();
         out.close();
